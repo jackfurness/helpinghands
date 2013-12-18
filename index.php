98d976e41d1854535e9d0b895be3576b1574ae0d
Jack Furness <jackfurness@jacks-air.default>
2013-12-18 23:05:43 +0000

    Added new sections to homepage

--- index.php
+++ index.php
@@ -3,6 +3,13 @@
 	'title'=>'Homepage'
 ));?>
 
+<?php $recentBlogs = array(
+    'count'=>'3',
+    'sort-order'=>'DESC',
+    'template'=>'blog/recent-blogs.html'
+    );
+?>
+
 <section class="banner">
 	<div class="container">
 		<div class="row">
@@ -11,6 +18,59 @@
 			</div>
 		</div>
 	</div>
+</section>
+<section class="welcome">
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12">
+				<h3 class="section-heading">Karibu! Welcome</h3>
+				<div class="col-xs-12 col-sm-4">
+				</div>
+				<div class="col-xs-12 col-sm-4">
+				</div>
+				<div class="col-xs-12 col-sm-4">
+				</div>
+			</div>
+		</div>
+	</div>
 </section>	
+<section class="recent-blogs">
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12">
+				<h3 class="section-heading">Recent blog entries</h3>
+				<div class="row">
+					<?php perch_blog_custom($recentBlogs);?>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<section class="get-involved">
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12">
+				<h3 class="section-heading">Get involved</h3>
+				<div class="row">
+					<div class="col-xs-12 col-sm-4">
+						<div class="contact-bubble"><span class="icon-bubbles"></span></div>
+						<h4>Keep in touch</h4>
+						<p>We have many ways for you to keep in touch with the latest that is happening at Helping Hands. Read our blog, newsletter or follow us on Facebook.</p>
+					</div>
+					<div class="col-xs-12 col-sm-4">
+						<div class="contact-bubble"><span class="icon-gbp"></span></div>
+						<h4>Donate</h4>
+						<p>We rely on the support of individuals around the world to keep the project going and moving on. We welcome donations of anything from money to equipment and supplies.</p>
+					</div>
+					<div class="col-xs-12 col-sm-4">
+						<div class="contact-bubble"><span class="icon-plane"></span></div>
+						<h4>Volunteer</h4>
+						<p>Helping Hands always needs people to sponsor centre participants, programs and employees. In addition volunteers who bring their expertise and skill to our programs have always been a mainstay of our success.</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>		
 
 <?php perch_layout('global.footer'); ?>
